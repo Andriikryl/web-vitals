8ebbedb8c6a37bf5bf09104ad84dd98a66d22a1c
Andriikryl <andreyk557@gmail.com>
2023-09-26 14:21:46 +0300

    add considering elements

--- src/components/lcp/Lcp.tsx
+++ src/components/lcp/Lcp.tsx
@@ -4,6 +4,15 @@ import CoreSet from "../coreSet/CoreSet";
 import Qast from "../Qast/Qast";
 
 const data = ["good", "needs improvment", "poor"];
+const LcpEllements = [
+  "<img> elements",
+  "<image> elements inside an <svg> element",
+  "<video> elements with a poster image (the poster image load time is used)",
+  "An element with a background image loaded via the url() function (as opposed to a CSS gradient)",
+  "Block-level elements containing text nodes or other inline-level text elements children.",
+  "The first frame painted for autoplaying <video> elements (as of August 2023)",
+  "The first frame of an animated image format, such as animated GIFs (as of August 2023)",
+];
 const answer =
   " Largest Contentful Paint (LCP) is an important, stable Core Web Vital metric for measuring perceived load speed because it marks the point in the page load timeline when the pages main content has likely loaded—a fast LCP helps reassure the user that the page is useful.";
 
@@ -22,7 +31,17 @@ export default function Lcp() {
         measures loading performance. To provide a good user experience, LCP
         should occur within 2.5 seconds of when the page first starts loading.
       </p>
-      <Qast qast="So why LCP importent" answer={answer} />
+      <Qast qast="So why LCP importent ?" answer={answer} />
+      <h3 className={style.elements__title}>What elements are considered?</h3>
+      <ul className={style.ellements__list} role="list">
+        {LcpEllements.map((item, index) => {
+          return (
+            <li className={style.ellement__item} key={index}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
